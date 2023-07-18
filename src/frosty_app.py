b32bed6217f00fefdbe0582531c24f898cba2e7b
Changliang Tan <121930227+sfc-gh-chtan@users.noreply.github.com>
2023-07-18 22:59:08 +0900

    Update frosty_app.py
    Comment out the if continue statement

--- src/frosty_app.py
+++ src/frosty_app.py
@@ -18,8 +18,8 @@ if prompt := st.chat_input():
 
 # display the existing chat messages
 for message in st.session_state.messages:
-    if message["role"] == "system":
-        continue
+    #if message["role"] == "system":
+    #    continue
     with st.chat_message(message["role"]):
         st.write(message["content"])
         if "results" in message:
